40ed8213dd5b25ea248afaddcb69fbef85fd45b0
Marshall <mars@studiocydonia.com>
2025-11-05 17:59:24 -0500

    Quartz sync: Nov 5, 2025, 5:59 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -87,8 +87,11 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
-      // Comment out CustomOgImages to speed up build time
-      // Plugin.CustomOgImages(),
+      Plugin.CustomOgImages({
+        colorScheme: "darkMode",
+        width: 800,
+        height: 300
+      }),
     ],
   },
 }
